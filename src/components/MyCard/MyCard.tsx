37bff063621190dacc7aea5208767ddbc4834d98
Finsoy <vadimka.cry.00@gmail.com>
2021-08-30 15:24:59 +0300

    feat: add ICardsDataDTO and use uuid

--- src/components/MyCard/MyCard.tsx
+++ src/components/MyCard/MyCard.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import './MyCard.scss';
 import {Card, CardActionArea, CardContent, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 interface ICardProps {
     headerText: string;
     bodyText: string;
 }
 
+const useStyles = makeStyles({
+    card: {
+    maxWidth: "250px",
+    width: "100%",
+    margin: "1rem",
+    cursor: "pointer",
+    }
+});
+
 const MyCard = ({headerText, bodyText}: ICardProps) => {
+    const classes = useStyles();
+
     return (
-            <Card className="card">
+            <Card className={classes.card}>
                 <CardActionArea>
                     <CardContent>
                         <Typography variant="h5" color="primary" gutterBottom>
--- src/states/App.tsx
+++ src/states/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { v4 } from 'uuid';
 import {makeStyles} from '@material-ui/core/styles';
 import {Container} from "@material-ui/core";
 import MyCard from "../components/MyCard/MyCard";
 import Header from "../components/Header/Header";
 import arrayCards from '../data/arrayCards.json'
+import ICardsDataDTO from "../data/ICardsDataDTO";
 
 const useStyles = makeStyles({
     cardContainer: {
@@ -19,8 +21,9 @@ const App = () => {
         <div>
             <Header/>
             <Container fixed className={classes.cardContainer}>
-                {arrayCards.map(({title, body}) => {
-                    return <MyCard headerText={title} bodyText={body}/>
+                {arrayCards.map(({title, body, id}: ICardsDataDTO) => {
+                    id = v4()
+                    return <MyCard headerText={title} bodyText={body} key={id}/>
                 })}
             </Container>
         </div>
