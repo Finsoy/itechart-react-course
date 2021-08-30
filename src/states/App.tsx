7bff07a48c0a5a706d0425b41ee717960b95f718
Finsoy <vadimka.cry.00@gmail.com>
2021-08-30 17:00:38 +0300

    feat: add directory models

--- src/states/App.tsx
+++ src/states/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { v4 } from 'uuid';
+import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import {Container} from "@material-ui/core";
 import MyCard from "../components/MyCard/MyCard";
 import Header from "../components/Header/Header";
 import arrayCards from '../data/arrayCards.json'
-import ICardsDataDTO from "../data/ICardsDataDTO";
+import ICardsDataDTO from "../models/ICardsDataDTO";
 
 const useStyles = makeStyles({
     cardContainer: {
@@ -17,12 +16,13 @@ const useStyles = makeStyles({
 
 const App = () => {
     const classes = useStyles();
+    const [cards] = useState<Array<ICardsDataDTO>>(arrayCards)
+
     return (
         <div>
             <Header/>
             <Container fixed className={classes.cardContainer}>
-                {arrayCards.map(({title, body, id}: ICardsDataDTO) => {
-                    id = v4()
+                {cards.map(({title, body, id}) => {
                     return <MyCard headerText={title} bodyText={body} key={id}/>
                 })}
             </Container>
