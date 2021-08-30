dfd69004a87daa282b3b7dcd3231bd36ca626fbd
Finsoy <vadimka.cry.00@gmail.com>
2021-08-30 19:58:05 +0300

    feat: add fetchData()

--- src/states/App.tsx
+++ src/states/App.tsx
@@ -1,10 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import {Button, Container} from "@material-ui/core";
 import MyCard from "../components/MyCard/MyCard";
 import Header from "../components/Header/Header";
 import MyModal from "./MyModal/MyModal";
-import arrayCards from '../data/arrayCards.json'
 import ICardsDataDTO from "../models/ICardsDataDTO";
 
 const useStyles = makeStyles({
@@ -23,8 +22,20 @@ const App = () => {
     const [cardHeaderText, setCardHeaderText] = useState<string>('')
     const [cardBodyText, setCardBodyText] = useState<string>('')
     const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [cards, setCards] = useState<ICardsDataDTO[]>(arrayCards)
+    const [cards, setCards] = useState<ICardsDataDTO[]>([])
     const classes = useStyles();
+
+    async function fetchData() {
+        let data: ICardsDataDTO[] = [];
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_page=1&_limit=2')
+        await response.json().then(res => data = res)
+        console.log(data)
+        setCards(data)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     const handleOpen = () => {
         setIsOpen(true);
