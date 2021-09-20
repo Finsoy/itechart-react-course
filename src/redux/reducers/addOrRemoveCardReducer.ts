9063e65ff14de976329eedeab67522298e5c6fcc
Finsoy <vadimka.cry.00@gmail.com>
2021-09-20 19:30:48 +0300

    fix: some changes from PR

--- src/redux/reducers/addOrRemoveCardReducer.ts
+++ src/redux/reducers/addOrRemoveCardReducer.ts
@@ -5,7 +5,7 @@ interface IAddOrRemoveCardAction {
     type: string;
     id?: string;
     cards: ICardsDataDTO[];
-    setCards: (cards: ICardsDataDTO[]) => void
+    setCards: (cards: (ICardsDataDTO)[]) => void
     cardHeaderText?: string;
     cardBodyText?: string;
 }
@@ -16,27 +16,8 @@ const addOrRemoveCardReducer = (
 ) => {
     switch (action.type) {
         case "ADD": {
-            fetch("https://jsonplaceholder.typicode.com/posts", {
-                method: "POST",
-                body: JSON.stringify({
-                    title: action.cardHeaderText,
-                    body: action.cardBodyText,
-                }),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8",
-                },
-            })
-                .then((response) => {
-                    console.log(`STATUS ${response.status}`);
-                    return response.json();
-                })
-                .then(async (json) => {
-                    json.id = v4();
-                    action.setCards([...action.cards, json])
-                    return;
-                })
-                .catch((e) => console.error(e));
-            break;
+            action.setCards([...action.cards, {title: action.cardHeaderText!, body: action.cardBodyText!, id: v4()}])
+            return;
         }
         case "REMOVE": {
             action.setCards(action.cards.filter((item) => item.id !== action.id));
